ef54a8fbba98da34db5a770303c1ceab400183ee
celestealejandra <78970530+celestealejandra@users.noreply.github.com>
2021-10-21 23:18:08 -0500

    Add files via upload

--- PRACTICA R 2.R
+++ PRACTICA R 2.R
@@ -23,9 +23,44 @@ suicidios_2018 <- read_excel("~/suicidios_2018.xlsx",
   "numeric", "numeric", "numeric"))
 clean_names(suicidios_2018)
 
-# ☼ LECTURA DE DATOS Y ESTIMACIONES ☼ 
+# ☼ LECTURA DE DATOS ☼ 
+names(suicidios_2018)
+suicidios_2018$suicidios_total
 head(suicidios_2018)
+suicidios_2018$suicidios_total
+
+
 summary(suicidios_2018$suicidios_total)
 summary(suicidios_2018$suicidios_H)
 summary(suicidios_2018$suicidios_M)
+
+# ☼ MEDIDAS DE TENDENCIA CENTRAL☼ 
+#RANGO
+range(x=suicidios_2018$suicidios_total, na.rm = TRUE)
+
+#MEDIA DE SUICIDIOS EN MUJERES A NIVEL NACIONAL PARA 2018
+mean(x=suicidios_2018$suicidios_M, na.rm = TRUE )
+#MEDIA DE SUICIDIOS EN HOMBRES A NIVEL NACIONAL PARA 2018
+mean(x=suicidios_2018$suicidios_H, na.rm = TRUE)
+#MEDIA DE SUICIDIOS TOTALES A NIVEL NACIONAL PARA 2018
+mean(x=suicidios_2018$suicidios_total, na.rm=TRUE)
+
+#MODA
+table(suicidios_2018$suicidios_total)
+
+#MEDIANA PARA SUICIDIOS TOTALES A NIVEL NACIONAL
+median(x=suicidios_2018$suicidios_total, na.rm = TRUE)
+
+#DESVIACIÓN ESTANDAR
+sd(x=suicidios_2018$suicidios_total, na.rm = TRUE)
+sd(x=suicidios_2018$suicidios_H, na.rm = TRUE)
+sd(x=suicidios_2018$suicidios_M, na.rm = TRUE)
+
+#COEFICIENTE DE VARIABILIDAD 
+"coeficiente de variabilidad s_h" <- function(x=suicidios_2018, na.rm= TRUE){
+  sd(x= suicidios_2018$suicidios_H, na.rm=na.rm) / mean(x=suicidios_2018$suicidios_H, na.rm=na.rm)
+}
+`coeficiente de variabilidad s_h`()
+
+
 
